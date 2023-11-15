fff0172ef782f0ba9868a40d99840fe0f134afee
PATRICK <patrick.kinyua@jujapreparatory.ac.ke>
2023-11-15 13:18:32 +0300

    server update

--- Farms/urls.py
+++ Farms/urls.py
@@ -1,22 +1,20 @@
-from django.conf.urls import url
 from Farms import views
-from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^farmer$', views.FarmerApi),
-    url(r'^farmer/([0-9]+)$', views.FarmerApi),
-    url(r'^farm$', views.FarmApi),
-    url(r'^farm/([0-9]+)$', views.FarmApi),
-    url(r'^farmer-login$', views.FarmerLoginApi),
-    url(r'^farmer-login/([0-9]+)$', views.FarmerLoginApi),
-    url(r'^farmdata$', views.FarmDataApi),
-    url(r'^farmdata/([0-9]+)$', views.FarmDataApi),
-    url(r'^wificreds$', views.FarmDataApi),
-    url(r'^wificreds/([0-9]+)$', views.FarmDataApi),
+    path(r'^farmer$', views.FarmerApi),
+    path(r'^farmer/([0-9]+)$', views.FarmerApi),
+    path(r'^farm$', views.FarmApi),
+    path(r'^farm/([0-9]+)$', views.FarmApi),
+    path(r'^farmer-login$', views.FarmerLoginApi),
+    path(r'^farmer-login/([0-9]+)$', views.FarmerLoginApi),
+    path(r'^farmdata$', views.FarmDataApi),
+    path(r'^farmdata/([0-9]+)$', views.FarmDataApi),
+    path(r'^wificreds$', views.FarmDataApi),
+    path(r'^wificreds/([0-9]+)$', views.FarmDataApi),
 
 ]
